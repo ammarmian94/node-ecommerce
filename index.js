9f7cad32378cfbe7002fd67bcab07bed97815938
ammarmian94 <96662437+ammarmian94@users.noreply.github.com>
2023-12-08 21:48:22 +0500

    Backend part 5. Authorization done

--- index.js
+++ index.js
@@ -9,21 +9,114 @@ const UserRouter = require("./routes/User");
 const AuthRouter = require("./routes/Auth");
 const CartRouter = require("./routes/Cart");
 const OrderRouter = require("./routes/Order");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const jwt = require("jsonwebtoken");
+const { User } = require("./model/User");
+const crypto = require("crypto");
+const { isAuth, sanitizeUser } = require("./services/common");
 
+const SECRET_KEY = "SECRET_KEY";
+
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = SECRET_KEY;
 // middlewares
+server.use(
+  session({
+    secret: "keyboard cat",
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+  })
+);
+server.use(passport.authenticate("session"));
+
 server.use(
   cors({
     exposedHeaders: ["X-Total-Count"],
   })
 );
 server.use(express.json());
-server.use("/products", ProductRouter.router);
-server.use("/categories", CategoryRouter.router);
-server.use("/brands", BrandRouter.router);
-server.use("/users", UserRouter.router);
+server.use("/products", isAuth(), ProductRouter.router);
+server.use("/categories", isAuth(), CategoryRouter.router);
+server.use("/brands", isAuth(), BrandRouter.router);
+server.use("/users", isAuth(), UserRouter.router);
 server.use("/auth", AuthRouter.router);
-server.use("/cart", CartRouter.router);
-server.use("/orders", OrderRouter.router);
+server.use("/cart", isAuth(), CartRouter.router);
+server.use("/orders", isAuth(), OrderRouter.router);
+
+// Passport Strategy Local
+passport.use(
+  "local",
+  new LocalStrategy(
+    { usernameField: "email", passwordField: "password" },
+    async function (email, password, done) {
+      try {
+        const user = await User.findOne({ email: email }).exec();
+        if (!user) {
+          return done(null, false, { message: "Invalid Credentials" });
+        }
+
+        var salt = crypto.randomBytes(16);
+        crypto.pbkdf2(
+          password,
+          user.salt,
+          310000,
+          32,
+          "sha256",
+          async function (err, hashedPassword) {
+            if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+              return done(null, false, { message: "Invalid Credentials" });
+            } else {
+              const token = jwt.sign(sanitizeUser(user), SECRET_KEY);
+              return done(null, token); //this call serializeUser
+            }
+          }
+        );
+      } catch (error) {
+        console.log("catch");
+        done(error);
+      }
+    }
+  )
+);
+
+// Passport Strategy JWT
+passport.use(
+  "jwt",
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    try {
+      const user = await User.findOne({ id: jwt_payload.sub });
+      if (user) {
+        return done(null, sanitizeUser(user)); //this calls serialize;
+      } else {
+        return done(null, false);
+      }
+    } catch (error) {
+      return done(err, false);
+    }
+  })
+);
+
+// this creates session variable req.user on being called from callbacks
+passport.serializeUser(function (user, cb) {
+  // console.log("serialize", user);
+  process.nextTick(function () {
+    return cb(null, { id: user.id, role: user.role });
+  });
+});
+
+// this creates session variable req.user when called from authorized user
+// called on every authorized call
+passport.deserializeUser(function (user, cb) {
+  // console.log("de-serialize", user);
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
 
 main().catch((err) => console.log(err));
 
--- routes/Auth.js
+++ routes/Auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, loginUser } = require("../controllers/Auth");
+const passport = require("passport");
+const { createUser, loginUser, checkUser } = require("../controllers/Auth");
 
-router.post("/signup", createUser).post("/login", loginUser);
+router
+  .post("/signup", createUser)
+  .post("/login", passport.authenticate("local"), loginUser)
+  .get("/check", passport.authenticate("jwt"), checkUser);
 
 exports.router = router;
